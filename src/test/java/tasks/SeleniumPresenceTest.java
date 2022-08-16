f9cd58bd958081a8f032c3caafeb55651cfb0798
plmaskw2 <maciek115606@gmail.comn>
2022-08-17 00:53:47 +0200

    added options to browser

--- src/test/java/tasks/SeleniumPresenceTest.java
+++ src/test/java/tasks/SeleniumPresenceTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
@@ -16,7 +17,10 @@ public class SeleniumPresenceTest {
     @BeforeClass
     static void setupAll() {
         WebDriverManager.getInstance(ChromeDriver.class).driverVersion("104.0.5112.79").setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        WebDriver driver = new ChromeDriver(options);
         browser.set(driver);
     }
 
