f7fdbb2e745effacb02f578c09ba72f383f19a50
plmaskw2 <maciek115606@gmail.comn>
2022-08-17 01:00:53 +0200

    added options

--- src/test/java/tasks/SeleniumPresenceTest.java
+++ src/test/java/tasks/SeleniumPresenceTest.java
@@ -20,6 +20,7 @@ public class SeleniumPresenceTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+        options.addArguments("--remote-debugging-port=9222");
         options.addArguments("--disable-dev-shm-usage");
         WebDriver driver = new ChromeDriver(options);
         browser.set(driver);
