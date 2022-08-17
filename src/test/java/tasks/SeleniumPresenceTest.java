82670cf35c878facc00a2781d4119c4f27b3d0c4
plmaskw2 <maciek115606@gmail.comn>
2022-08-17 02:33:25 +0200

    fix

--- src/test/java/tasks/SeleniumPresenceTest.java
+++ src/test/java/tasks/SeleniumPresenceTest.java
@@ -24,11 +24,6 @@ public class SeleniumPresenceTest {
         WebDriverManager.getInstance(ChromeDriver.class).driverVersion("104.0.5112.79").setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
         WebDriver driver = new ChromeDriver(options);
         browser.set(driver);
     }
