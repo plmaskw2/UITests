ec1e9a2c115100b56d8aca206e8cf3052fd759ba
plmaskw2 <maciek115606@gmail.comn>
2024-04-30 16:31:47 +0200

    Attach video

--- src/test/java/base/AfterTestExecutionCallbackBase.java
+++ src/test/java/base/AfterTestExecutionCallbackBase.java
@@ -1,11 +1,17 @@
 package base;
 
+import framework.utils.ConfigurationUtils;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
+import org.apache.commons.compress.utils.IOUtils;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.*;
 
 public class AfterTestExecutionCallbackBase implements AfterTestExecutionCallback {
     @Override
@@ -16,6 +22,20 @@ public class AfterTestExecutionCallbackBase implements AfterTestExecutionCallbac
             takeScreenshot(driver);
         }
         driver.quit();
+        if (ConfigurationUtils.properties.getProperty("driverType").equals("REMOTE")) {
+            attachVideo(driver);
+        }
+    }
+
+    @Attachment(value = "Video", type = "video/mp4")
+    public static byte[] attachVideo(WebDriver driver) {
+        byte[] byteArr = null;
+        try {
+            byteArr = IOUtils.toByteArray(new FileInputStream("http://192.168.0.121:5555/video/%s.mp4".formatted(((RemoteWebDriver) driver).getSessionId().toString())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return byteArr;
     }
 
     @Attachment(value = "Screenshot", type = "image/png")
