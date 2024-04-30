0f3c8aa30b0e612752677087b76d70fc016b3217
plmaskw2 <maciek115606@gmail.comn>
2024-04-30 02:28:33 +0200

    attempt1

--- src/test/java/base/AfterTestExecutionCallbackBase.java
+++ src/test/java/base/AfterTestExecutionCallbackBase.java
@@ -10,26 +10,37 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.Augmenter;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.function.Supplier;
 
 public class AfterTestExecutionCallbackBase implements AfterTestExecutionCallback {
     @Override
     public void afterTestExecution(ExtensionContext context) {
         WebDriver driver = DriverFactory.valueOf(ConfigurationUtils.properties.getProperty("driver")).getDriverManager().getDriver();
         if (context.getExecutionException().isPresent()) {
-            takeScreenshot(driver);
+            try {
+                Allure.addAttachment("Screenshot", FileUtils.openInputStream(getScreenshot(driver)));
+            }
+            catch (IOException ex) {}
         }
     }
 
-    private void takeScreenshot(WebDriver driver) {
-        if (driver instanceof TakesScreenshot) {
-            byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            Allure.addByteAttachmentAsync("Screenshot", "image/png", "png", (Supplier<byte[]>) new ByteArrayInputStream(screenshotBytes));
+    @Attachment(value = "HTML attachment", type = "text/html")
+    private File getScreenshot(WebDriver driver) {
+        String image = null;
+        Augmenter augmenter = new Augmenter();
+        TakesScreenshot ts = (TakesScreenshot) augmenter.augment(driver);
+        File src = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            image = System.getProperty("user.dir") + "\\screenshots\\" + "screenshot.png";
+            FileUtils.copyFile(src, new File(image));
         }
+        catch (IOException ex) {
+            ex.getStackTrace();
+        }
+        return src;
     }
 }
