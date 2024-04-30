9af37f88ee5d02a8cae92d52d25f3fa03e13a2cd
plmaskw2 <maciek115606@gmail.comn>
2024-04-30 16:34:42 +0200

    Fix to videos

--- src/test/java/base/AfterTestExecutionCallbackBase.java
+++ src/test/java/base/AfterTestExecutionCallbackBase.java
@@ -1,7 +1,6 @@
 package base;
 
 import framework.utils.ConfigurationUtils;
-import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.apache.commons.compress.utils.IOUtils;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
@@ -10,6 +9,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
 
 import java.io.*;
 
@@ -18,20 +18,21 @@ public class AfterTestExecutionCallbackBase implements AfterTestExecutionCallbac
     public void afterTestExecution(ExtensionContext context) {
         Object testInstance = context.getRequiredTestInstance();
         WebDriver driver = ((WebBaseTestJUnit) testInstance).driver;
+        SessionId sessionId = ((RemoteWebDriver) driver).getSessionId();
         if (context.getExecutionException().isPresent()) {
             takeScreenshot(driver);
         }
         driver.quit();
         if (ConfigurationUtils.properties.getProperty("driverType").equals("REMOTE")) {
-            attachVideo(driver);
+            attachVideo(sessionId);
         }
     }
 
     @Attachment(value = "Video", type = "video/mp4")
-    public static byte[] attachVideo(WebDriver driver) {
+    public static byte[] attachVideo(SessionId sessionId) {
         byte[] byteArr = null;
         try {
-            byteArr = IOUtils.toByteArray(new FileInputStream("http://192.168.0.121:5555/video/%s.mp4".formatted(((RemoteWebDriver) driver).getSessionId().toString())));
+            byteArr = IOUtils.toByteArray(new FileInputStream("http://192.168.0.121:5555/video/%s.mp4".formatted(sessionId.toString())));
         } catch (IOException e) {
             e.printStackTrace();
         }
