aa48df1cddcdd8a1529d268687e2d7da32f7c444
plmaskw2 <maciek115606@gmail.comn>
2024-05-01 01:54:22 +0200

    REFACTOR

--- src/main/java/framework/utils/ConfigurationUtils.java
+++ src/main/java/framework/utils/ConfigurationUtils.java
@@ -8,16 +8,22 @@ import java.util.Properties;
 public class ConfigurationUtils {
 
     public static Properties properties = new Properties();
-    public  static boolean isRemote = getProperty("driverType").equals("REMOTE");
-    public static boolean isSelenoid = getProperty("selenoid").equals("true");
-    public static final String DRIVER_TYPE = getProperty("driverType");
-    public static final String SELENOID_HUB = getProperty("selenoidHub");
-    public static final String GRID_HUB = getProperty("gridHub");
-    public static boolean isEnableVideo = getProperty("enableVideo").equals("true");
+    public static boolean isRemote;
+    public static boolean isSelenoid;
+    public static String DRIVER_TYPE;
+    public static String SELENOID_HUB;
+    public static String GRID_HUB;
+    public static boolean isEnableVideo;
 
     @SneakyThrows
     public static Properties loadProperties() {
         properties.load(new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/" + "configuration.properties"));
+        isRemote = getProperty("driverType").equals("REMOTE");
+        isSelenoid = getProperty("selenoid").equals("true");
+        DRIVER_TYPE = getProperty("driverType");
+        SELENOID_HUB = getProperty("selenoidHub");
+        GRID_HUB = getProperty("gridHub");
+        isEnableVideo = getProperty("enableVideo").equals("true");
         return properties;
     }
 
